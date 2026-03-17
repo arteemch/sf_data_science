53940b26c56379ba5b59cb6faca4f4a0b6919253
Artem <cherednikov.ad20@gmail.com>
2026-03-17 14:48:57 +0300

    обновлены код программы и README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+# My data science projects
+# Алгоритм угадывает заданное число от 1 до 100 методом бинарного поиска.
+
+## Установка
+```bash
+pip install -r requirements.txt
+
+
+
+import game_v3
+
+# Запуск игры
+print(f"Попыток: {game_v3.game_core_v3()}")
--- project_0/game_v3.py
+++ project_0/game_v3.py
@@ -1,15 +1,16 @@
 import numpy as np
 
-def game_core_v3(number: int = 1) -> int:
+def game_core_v3(number: int = None) -> int:
     """
     Угадывает число бинарным поиском
     Args:
-        number (int, optional): Загаданное число. Defaults to 1.
+        number (int, optional): Загаданное число. Defaults to None. Если число не задается, оно выбирается случайным образом
 
     Returns:
         int: Число попыток
     """
-            
+    if number is None:
+        number = np.random.randint(1,101)        
     count = 0
     low, high = 1, 100    # Устанавливаем границы динамического диапазона, который будет сужаться с каждой новой попыткой 
 
@@ -25,5 +26,31 @@ def game_core_v3(number: int = 1) -> int:
         else:
             high = predict - 1  # Ищем в нижней половине
     
+    
 
-game_core_v3(67)
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем за 10000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(10000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Алгоритм угадывает число в среднем за: {score} попыток")
+    
+    
+        
+print(f'Количество попыток: {game_core_v3()}')
+score_game(game_core_v3)
+
+
+
